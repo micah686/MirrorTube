64fac44001bcc16d3a8f1b331b52b0390eb78761
Micah <micah686@github.com>
2023-03-11 14:08:16 -0800

    Added yt-dlp

--- src/MirrorTube/MirrorTube.API/StartupChecks.cs
+++ src/MirrorTube/MirrorTube.API/StartupChecks.cs
@@ -10,11 +10,23 @@ namespace MirrorTube.API
             {
                 SecureStore.CreateSecureStore();
             }
+            DownloadBinaries();
         }
 
         private static void CreateDirectories()
         {
             Directory.CreateDirectory(Globals.ServerDataPath);            
+        }
+
+        private static void DownloadBinaries()
+        {
+            var ytdlp = Path.Combine(Globals.ServerDataPath, YoutubeDLSharp.Utils.YtDlpBinaryName);
+            var ffmpeg = Path.Combine(Globals.ServerDataPath, YoutubeDLSharp.Utils.FfmpegBinaryName);
+            var ffprobe = Path.Combine(Globals.ServerDataPath, YoutubeDLSharp.Utils.FfprobeBinaryName);
+
+            if (!File.Exists(ytdlp)) { YoutubeDLSharp.Utils.DownloadYtDlp(Path.GetDirectoryName(ytdlp)); }
+            if (!File.Exists(ffmpeg)) { YoutubeDLSharp.Utils.DownloadFFmpeg(Path.GetDirectoryName(ffmpeg)); }
+            if (!File.Exists(ffprobe)) { YoutubeDLSharp.Utils.DownloadFFprobe(Path.GetDirectoryName(ffprobe)); }
         }
     }
 }
